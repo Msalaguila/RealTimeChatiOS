72550bd1fbb47d09d36978374640cae4488798e1
Manuel <msalaguila@gmail.com>
2019-06-16 11:37:37 +0200

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by Manuel Salvador del Águila on 16/06/2019.
+//  Copyright © 2019 Manuel Salvador del Águila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
